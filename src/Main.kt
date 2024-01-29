4cbc304fc0c74b17b33489565a3fddaea35bc7d3
anaszahid <anaszahid572@gmail.com>
2024-01-29 10:28:12 +0000

    28 JANUARY

--- src/Main.kt
+++ src/Main.kt
@@ -25,21 +25,17 @@ fun main() {
     // This mechanism applies when an interpreter
     // is executing logical expressions, and skips the "irrelevant" parts of the expressions.
 
-    var i = 15
-    var j = 18
-
-    var result = i == 16 || j++ == 18
-    println(i)
-    println(j)
+//    var i = 15
+//    var j = 18
+//
+//    var result = i == 16 || j++ == 18
+//    println(i)
+//    println(j)
 
 
     // ! NOT
     // false --> ! --> true
     // true --> ! --> false
-
-//    val answer = true
-//    result = !answer
-//     println(result)
 
 
         val isRaining = false
@@ -50,5 +46,91 @@ fun main() {
         else{
             println("Take Umbrella")
         }
+
+
+        val x= 15
+        val y = 20
+
+        if (x > y){
+           println("$x Is Greater Than $y")
+        } else if (x < y) {
+            println("$x Is Smaller Than $y")
+        }else{
+           println("$x Is Equal To $y")
+        }
+
+
+        // If And Else Can Be Used As An Expression
+        val a = 20
+        val b = 15
+
+        if (a > b){
+            println("A Is Greater Than B")
+        }else if (a < b){
+            println("A Is Smaller Than B")
+        }else{
+            println("A Is Equal To B")
+        }
+
+        val c = 18
+        val d = 18
+
+        if (c > d){
+            println("C Is Greater Than D")
+        }else if (c < d){
+            println("C Is Smaller Than D")
+        }else{
+            println("C Is Equal To D")
+        }
+    // x > y, x < y, x==y
+
+
+//        val number = 19
+//        if (number % 2 == 0){
+//            println("Number Is Even & Can Be Divided By 2")
+//        }else{
+//            println("Number Is Odd & Cannot Be Divided By 2")
+//        }
+
+//    val number = 88
+//    val result = number in 1 until 88
+//    println(result)
+
+
+    val animal = "Dog"
+    // When Can Also Be Used As An Expression Like If & Else
+    when (animal){
+        "Horse" -> println("Animal Is Horse")
+        "Cat" -> println("Animal Is Cat")
+        "Dog" -> println("Animal Is Dog")
+        else -> println("Animal Not Found")
+    }
+
+    val digit = 19
+
+    when (digit){
+        13 -> println("Thirteen")
+        14 -> println("Fourteen")
+        15 -> println("Fifteen")
+        16 -> println("Sixteen")
+        in 17..19 -> println("Teen")
+        else -> println("Not In Range")
+    }
+
+    // What Are Loops?
+    // Loops Are Set Of Statements Which You Want To Be Executed Multiple Times
+
+    var count = 1
+    while (count <= 50)
+    {
+        println("This Bomb Will Go Off In  $count" ) // Concatenation
+        count++
+    }
+
+
+
+
+
+
     }
 
